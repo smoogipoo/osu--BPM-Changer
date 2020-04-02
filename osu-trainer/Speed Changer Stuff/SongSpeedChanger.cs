c23a8b44185087bc22fc0f5f6f85cd3c3c9c6173
Jun Villa <junarvi@yahoo.ca>
2020-04-01 20:30:32 -0400

    Use fast audio file generation

--- osu-trainer/Speed Changer Stuff/SongSpeedChanger.cs
+++ osu-trainer/Speed Changer Stuff/SongSpeedChanger.cs
@@ -11,13 +11,16 @@ namespace osu_trainer
 {
     class SongSpeedChanger
     {
-        // TODO: do we need all these steps? can we do something to improve performance?
-        // This function creates newMap.AudioFilename, assuming it does not exist yet.
-        // This function uses originalMap to determine the input audio file to process
+        // (in: originalMap.AudioFilename) => (out: newMap.AudioFilename)
         public static void GenerateAudioFile(Beatmap originalMap, Beatmap newMap, double multiplier)
         {
             if (multiplier == 1)
                 throw new ArgumentException("Don't call this function if multiplier is 1.0x");
+
+
+            var watch = new Stopwatch();
+            watch.Start();
+
             //temp1: Audio copy
             //temp2: Decoded wav
             //temp3: stretched file
@@ -33,7 +36,7 @@ namespace osu_trainer
             // lame.exe
             Process lame1 = new Process();
             lame1.StartInfo.FileName = "Speed Changer Stuff\\lame.exe";
-            lame1.StartInfo.Arguments = string.Format("--decode \"{0}\" \"{1}\"", temp1, temp2);
+            lame1.StartInfo.Arguments = string.Format("-q 9 --priority 4 --decode \"{0}\" \"{1}\"", temp1, temp2);
             lame1.StartInfo.UseShellExecute = false;
             lame1.StartInfo.CreateNoWindow = true;
             lame1.Start();
@@ -42,7 +45,7 @@ namespace osu_trainer
             // soundstretch.exe
             Process soundstretch = new Process();
             soundstretch.StartInfo.FileName = "Speed Changer Stuff\\soundstretch.exe";
-            soundstretch.StartInfo.Arguments = string.Format("\"{0}\" \"{1}\" -tempo={2}", temp2, temp3, (multiplier - 1) * 100);
+            soundstretch.StartInfo.Arguments = string.Format($"\"{temp2}\" \"{temp3}\" -quick -naa -tempo={(multiplier - 1) * 100}");
             soundstretch.StartInfo.UseShellExecute = false;
             soundstretch.StartInfo.CreateNoWindow = true;
             soundstretch.Start();
@@ -51,13 +54,16 @@ namespace osu_trainer
             // lame.exe again
             Process lame2 = new Process();
             lame2.StartInfo.FileName = "Speed Changer Stuff\\lame.exe";
-            lame2.StartInfo.Arguments = string.Format("\"{0}\" \"{1}\"", temp3, temp4);
+            lame2.StartInfo.Arguments = string.Format("-q 9 --priority 4 \"{0}\" \"{1}\"", temp3, temp4);
             lame2.StartInfo.UseShellExecute = false;
             lame2.StartInfo.CreateNoWindow = true;
             lame2.Start();
             lame2.WaitForExit();
 
             CopyFile(temp4, Path.GetDirectoryName(newMap.Filename) + "\\" + newMap.AudioFilename);
+
+            watch.Stop();
+            Console.WriteLine($"Time elapsed: {watch.ElapsedMilliseconds}");
 
             // Clean up
             File.Delete(temp1);
